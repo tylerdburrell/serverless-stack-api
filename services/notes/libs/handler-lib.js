4bf60cb5c72db3d107cf335e6fe2498e79810f86
Tyler <tylerdburrell@gmail.com>
2021-02-11 20:06:49 -0600

    Adding error logging

--- services/notes/libs/handler-lib.js
+++ services/notes/libs/handler-lib.js
@@ -1,24 +1,32 @@
+import * as debug from "./debug-lib";
+
 export default function handler(lambda) {
-    return async function (event, context) {
-      let body, statusCode;
+  return async function (event, context) {
+    let body, statusCode;
 
-      try {
-        // Run the Lambda
-        body = await lambda(event, context);
-        statusCode = 200;
-      } catch (e) {
-        body = { error: e.message };
-        statusCode = 500;
-      }
+    // Start debugger
+    debug.init(event, context);
 
-      // Return HTTP response
-      return {
-        statusCode,
-        body: JSON.stringify(body),
-        headers: {
-          "Access-Control-Allow-Origin": "*",
-          "Access-Control-Allow-Credentials": true,
-        },
-      };
+    try {
+      // Run the Lambda
+      body = await lambda(event, context);
+      statusCode = 200;
+    } catch (e) {
+      // Print debug messages
+      debug.flush(e);
+
+      body = { error: e.message };
+      statusCode = 500;
+    }
+
+    // Return HTTP response
+    return {
+      statusCode,
+      body: JSON.stringify(body),
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Credentials": true,
+      },
     };
-  }
+  };
+}
